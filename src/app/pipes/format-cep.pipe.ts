874e9d801891b9d1f7ef13f4bff7367c71346672
João Guilherme Gomes de Araújo <fabioejoaoguilherme503@hotmail.com>
2025-06-03 18:31:13 -0300

    Add src/app/pipes/format-cep.pipe.ts 1748986273389

---
+++ src/app/pipes/format-cep.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatCep',
+})
+export class FormatCepPipe implements PipeTransform {
+  transform(value: string): string {
+    if (!value) return '';
+    const cleaned = value.replace(/\D/g, '');
+    if (cleaned.length !== 8) return value;
+    return cleaned.replace(/(\d{5})(\d{3})/, '$1-$2');
+  }
+}
