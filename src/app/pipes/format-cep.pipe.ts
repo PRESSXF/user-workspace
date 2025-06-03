d898e77caeb0f014e78c148c16c1579fdf93b91b
João Guilherme Gomes de Araújo <fabioejoaoguilherme503@hotmail.com>
2025-06-03 19:47:14 -0300

    Add src/app/pipes/format-cep.pipe.ts 1748990834678

--- src/app/pipes/format-cep.pipe.ts
+++ src/app/pipes/format-cep.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'formatCep',
+  name: 'formatCep'
 })
 export class FormatCepPipe implements PipeTransform {
   transform(value: string): string {
