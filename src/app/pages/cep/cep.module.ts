09f5f290d2d79ff6e99ccf3a62984f1a9ccf8d43
João Guilherme Gomes de Araújo <fabioejoaoguilherme503@hotmail.com>
2025-06-03 18:31:17 -0300

    Add src/app/pages/cep/cep.module.ts 1748986277671

---
+++ src/app/pages/cep/cep.module.ts
@@ -0,0 +1,17 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CepPage } from './cep.page';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: CepPage }]),
+  ],
+  declarations: [CepPage],
+})
+export class CepPageModule {}
